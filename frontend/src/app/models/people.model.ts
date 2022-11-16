555736e5d31cdc8b6a08b8d0012fd0a44cb280f6
rabelorp <rabelo_rp@hotmail.com>
2022-11-16 17:13:12 -0300

    feat: models

---
+++ frontend/src/app/models/people.model.ts
@@ -0,0 +1,16 @@
+export class People {
+  id!: string;
+  name!: string;
+  lastName!: string;
+  nacionality!: string;
+  cpf!: string;
+  cep!: string;
+  state!: string;
+  city!: string;
+  public_place!: string;
+  email!: string;
+  telephone!: string;
+  currentPeople!: {};
+  people!: {};
+  data!: any;
+}
