118472f1f21e03b0a7895034ddfd780aa0343641
rabelorp <rabelo_rp@hotmail.com>
2022-11-16 17:16:31 -0300

    feat: app

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatePeopleComponent } from './components/create-people/create-people.component';
+import { DeletePeopleComponent } from './components/delete-people/delete-people.component';
+import { GetPeopleComponent } from './components/get-people/get-people.component';
+import { GetallPeopleComponent } from './components/getall-people/getall-people.component';
+import { UpdatePeopleComponent } from './components/update-people/update-people.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'people/getall', pathMatch: 'full' },
+  { path: 'people/create', component: CreatePeopleComponent },
+  { path: 'people/getall', component: GetallPeopleComponent },
+  { path: 'people/get/:id', component: GetPeopleComponent },
+  { path: 'people/update/:id', component: UpdatePeopleComponent },
+  { path: 'people/delete/:id', component: DeletePeopleComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CreatePeopleComponent } from './components/create-people/create-people.component';
+import { GetPeopleComponent } from './components/get-people/get-people.component';
+import { GetallPeopleComponent } from './components/getall-people/getall-people.component';
+import { UpdatePeopleComponent } from './components/update-people/update-people.component';
+import { DeletePeopleComponent } from './components/delete-people/delete-people.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatePeopleComponent,
+    GetPeopleComponent,
+    GetallPeopleComponent,
+    UpdatePeopleComponent,
+    DeletePeopleComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
